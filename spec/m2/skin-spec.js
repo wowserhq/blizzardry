e6291e76891ac73ede52dbb2ebe9bc6bdb22a10c
Tim Kurvers <tim@moonsphere.net>
2014-12-21 15:59:32 +0100

    Spec M2 skin

---
+++ spec/m2/skin-spec.js
@@ -0,0 +1,54 @@
+var Skin, expect, fixtures, fs, memo, r, sinon, util, _ref;
+
+_ref = require('../spec-helper'), expect = _ref.expect, fixtures = _ref.fixtures, memo = _ref.memo, sinon = _ref.sinon;
+
+fs = require('fs');
+
+util = require('util');
+
+r = require('restructure');
+
+Skin = require('../../lib/m2/skin');
+
+describe('Skin', function() {
+  var dummy;
+  dummy = memo().is(function() {
+    var data, stream;
+    data = fs.readFileSync(fixtures + 'Rabbit00.skin');
+    stream = new r.DecodeStream(data);
+    return Skin.decode(stream);
+  });
+  describe('#id', function() {
+    return xit('returns identifier');
+  });
+  describe('#indices', function() {
+    return it('returns indices', function() {
+      var indices;
+      indices = dummy().indices;
+      expect(indices).to.have.length(154);
+      expect(indices.slice(0, 4)).to.deep.eq([52, 58, 115, 0]);
+      return expect(indices.slice(-4)).to.deep.eq([29, 75, 76, 23]);
+    });
+  });
+  describe('#triangles', function() {
+    return it('returns triangles', function() {
+      var triangles;
+      triangles = dummy().triangles;
+      expect(triangles).to.have.length(107);
+      expect(triangles.slice(0, 2)).to.deep.eq([[0, 1, 2], [3, 4, 5]]);
+      return expect(triangles.slice(-2)).to.deep.eq([[150, 137, 151], [152, 148, 153]]);
+    });
+  });
+  describe('#properties', function() {
+    return xit('returns properties');
+  });
+  describe('#submeshes', function() {
+    return xit('returns submeshes');
+  });
+  describe('#textureUnits', function() {
+    return xit('returns texture units');
+  });
+  return describe('#bones', function() {
+    return xit('returns bones');
+  });
+});
