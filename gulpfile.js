bb19d1d76931b71c99149a1dcf50e84e820a00ec
Tim Kurvers <tim@moonsphere.net>
2015-06-12 23:12:40 +0200

    Drop semicolons

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var babel   = require('gulp-babel');
-var cache   = require('gulp-cached');
-var del     = require('del');
-var gulp    = require('gulp');
-var mocha   = require('gulp-mocha');
-var plumber = require('gulp-plumber');
+var babel   = require('gulp-babel')
+var cache   = require('gulp-cached')
+var del     = require('del')
+var gulp    = require('gulp')
+var mocha   = require('gulp-mocha')
+var plumber = require('gulp-plumber')
 
 gulp.task('clean', function(cb) {
   return del([
@@ -11,32 +11,32 @@ gulp.task('clean', function(cb) {
     'lib/*',
     'spec/**/*.js'
   ], cb)
-});
+})
 
 gulp.task('build', function() {
   return gulp.src('src/**/*.js')
     .pipe(cache('babel'))
     .pipe(plumber())
     .pipe(babel())
-    .pipe(gulp.dest('.'));
-});
+    .pipe(gulp.dest('.'))
+})
 
 gulp.task('spec', function() {
   return gulp.src('spec/**/*.js', {read: false})
     .pipe(plumber())
-    .pipe(mocha({bail: true}));
-});
+    .pipe(mocha({bail: true}))
+})
 
 gulp.task('rebuild', gulp.series(
   'clean', 'build'
-));
+))
 
 gulp.task('watch', function() {
   return gulp.watch('src/**/*.js', gulp.series(
     'build', 'spec'
-  ));
-});
+  ))
+})
 
 gulp.task('default', gulp.series(
   'rebuild', 'spec', 'watch'
-));
+))
