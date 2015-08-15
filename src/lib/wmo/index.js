655a8eefeaed6de91f52fa863e0859b59dae98a7
Tim Kurvers <tim@moonsphere.net>
2015-08-15 20:04:21 +0200

    Flesh out WMO root file parsing

--- src/lib/wmo/index.js
+++ src/lib/wmo/index.js
@@ -1,6 +1,7 @@
 const r = require('restructure');
 const Chunk = require('../chunked/chunk');
 const Chunked = require('../chunked');
+const SkipChunk = require('../chunked/skip-chunk');
 const {Vec3Float} = require('../types');
 
 const MOHD = Chunk({
@@ -11,7 +12,7 @@ const MOHD = Chunk({
   modelCount: r.uint32le,
   doodadCount: r.uint32le,
   doodadSetCount: r.uint32le,
-  colors: new r.Array(r.uint8, 4),
+  color: r.uint32le,
   wmoID: r.uint32le,
   minBoundingBox: Vec3Float,
   maxBoundingBox: Vec3Float,
@@ -23,9 +24,82 @@ const MOTX = Chunk({
   filenames: new r.Array(new r.String(null), 'size', 'bytes')
 });
 
+const MOMT = Chunk({
+  textures: new r.Array(new r.Struct({
+    flags: r.uint32le,
+    shader: r.uint32le,
+    blendMode: r.uint32le,
+
+    textures: new r.Array(new r.Struct({
+      offset: r.uint32le,
+      color: r.uint32le,
+      flags: r.uint32le
+    }), 3),
+
+    unknowns: new r.Reserved(r.uint32le, 4)
+  }), function() {
+    return this.parent.MOHD.textureCount;
+  })
+});
+
+const MOGN = Chunk({
+  names: new r.Array(new r.String(null), 'size', 'bytes')
+});
+
+const MOGI = Chunk({
+  groups: new r.Array(new r.Struct({
+    flags: r.uint32le,
+    minBoundingBox: Vec3Float,
+    maxBoundingBox: Vec3Float,
+    nameOffset: r.int32le
+  }), 'size', 'bytes')
+});
+
+const MOSB = Chunk({
+  skybox: new r.String('size')
+});
+
+const MODS = Chunk({
+  sets: new r.Array(new r.Struct({
+    name: new r.String(20),
+    startIndex: r.uint32le,
+    doodadCount: r.uint32le,
+    unused: new r.Reserved(r.uint32le)
+  }), 'size', 'bytes')
+});
+
+const MFOG = Chunk({
+  fogs: new r.Array(new r.Struct({
+    flags: r.uint32le,
+    position: Vec3Float,
+    smallerRadius: r.floatle,
+    largerRadius: r.floatle,
+    fogEnd: r.floatle,
+    fogStartMultiplier: r.floatle,
+    color: r.uint32le,
+    unknowns: new r.Reserved(r.floatle, 2),
+    color2: r.uint32le
+  }), 'size', 'bytes')
+});
+
 module.exports = Chunked({
   MOHD: MOHD,
   MOTX: MOTX,
+  MOMT: MOMT,
+  MOGN: MOGN,
+  MOGI: MOGI,
+  MOSB: MOSB,
+  MOPV: SkipChunk,
+  MOPT: SkipChunk,
+  MOPR: SkipChunk,
+  MOVV: SkipChunk,
+  MOVB: SkipChunk,
+  MOLT: SkipChunk,
+  MODS: MODS,
+  MODN: SkipChunk,
+  MODD: SkipChunk,
+  MFOG: MFOG,
+  // TODO: Optional MCVP chunk
 
   flags: function() {
     return this.MOHD.flags;
