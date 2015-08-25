13cb87c520590a312169cb1d4929a4a8035e889f
Tim Kurvers <tim@moonsphere.net>
2015-08-26 00:02:21 +0200

    Add MODF chunk

---
+++ src/lib/chunked/modf.js
@@ -0,0 +1,22 @@
+const r = require('restructure');
+const Chunk = require('./chunk');
+const {Vec3Float} = require('../types');
+
+module.exports = Chunk({
+  entries: new r.Array(new r.Struct({
+    index: r.uint32le,
+    id: r.uint32le,
+    position: Vec3Float,
+    rotation: Vec3Float,
+    minBoundingBox: Vec3Float,
+    maxBoundingBox: Vec3Float,
+    flags: r.uint16le,
+    doodadSet: r.uint16le,
+    nameSet: r.uint16le,
+    padding: new r.Reserved(r.uint16le),
+
+    filename: function() {
+      return this.parent.parent.MWMO.filenames[this.index];
+    }
+  }), 'size', 'bytes')
+});
