d39264cdf734fce5beacb81126dd6a594606c08e
Tim Kurvers <tim@moonsphere.net>
2015-12-09 23:52:31 +0100

    Add Quat type

--- src/lib/types/index.js
+++ src/lib/types/index.js
@@ -1,3 +1,4 @@
+export Quat from './quat';
 export Quat16 from './quat16';
 export Quat16Float from './quat16-float';
 export Vec3Float from './vec3-float';
---
+++ src/lib/types/quat.js
@@ -0,0 +1,8 @@
+import r from 'restructure';
+
+export default new r.Struct({
+  x: r.floatle,
+  y: r.floatle,
+  z: r.floatle,
+  w: r.floatle
+});
