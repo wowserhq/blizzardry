0c9b6e401a330259b6fb86fe9f57a6957661d19a
fallenoak <github@fallenoak.me>
2016-06-11 15:21:10 -0500

    Upgraded to Babel 6

--- src/spec/index-spec.js
+++ src/spec/index-spec.js
@@ -5,7 +5,7 @@ import { restructure } from '../lib';
 describe('Blizzardry', function() {
 
   it('exposes restructure', function() {
-    expect(restructure).to.eq(require('../lib/restructure'));
+    expect(restructure).to.eq(require('../lib/restructure').default);
     expect(restructure).to.eq(require('restructure'));
   });
 
