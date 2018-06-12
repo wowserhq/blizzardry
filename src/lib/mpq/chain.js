b359a04eb513f452258c7430c8ca4a322182180f
Tim Kurvers <tim@moonsphere.net>
2018-06-12 09:46:13 +0200

    Do not expose MPQ chain patterns

--- src/lib/mpq/chain.js
+++ src/lib/mpq/chain.js
@@ -4,26 +4,26 @@ import glob from 'globby';
 
 import MPQ from './';
 
-class MPQChain {
-  static PATTERNS = [
-    'common.MPQ',
-    'common-2.MPQ',
-    'expansion.MPQ',
-    'lichking.MPQ',
-    '*/locale-*.MPQ',
-    '*/speech-*.MPQ',
-    '*/expansion-locale-*.MPQ',
-    '*/lichking-locale-*.MPQ',
-    '*/expansion-speech-*.MPQ',
-    '*/lichking-speech-*.MPQ',
-    '*/patch-????.MPQ',
-    '*/patch-*.MPQ',
-    'patch.MPQ',
-    'patch-*.MPQ',
-  ];
+const PATTERNS = [
+  'common.MPQ',
+  'common-2.MPQ',
+  'expansion.MPQ',
+  'lichking.MPQ',
+  '*/locale-*.MPQ',
+  '*/speech-*.MPQ',
+  '*/expansion-locale-*.MPQ',
+  '*/lichking-locale-*.MPQ',
+  '*/expansion-speech-*.MPQ',
+  '*/lichking-speech-*.MPQ',
+  '*/patch-????.MPQ',
+  '*/patch-*.MPQ',
+  'patch.MPQ',
+  'patch-*.MPQ',
+];
 
+class MPQChain {
   static build(dataDir) {
-    const patterns = this.PATTERNS.map(mpq => (
+    const patterns = PATTERNS.map(mpq => (
       path.join(dataDir, mpq)
     ));
 
